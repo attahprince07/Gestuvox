6c72ed959ed50e10e0245554c54754ebc296b81d
Amoah Prince Kofi Attah <paamoah@wisc.edu>
2024-10-09 20:33:13 -0500

    create src

---
+++ src/data/teamMembers.js
@@ -0,0 +1,55 @@
+import AsumVictor from "../images/Profile/quality.png";
+import Prince from "../images/Profile/prince.jpg";
+// EDENIC
+import Morro from "../images/Profile/morro.jpeg";
+import Fibo from "../images/Profile/fibo.jpeg";
+
+
+export const TeamMembers = [
+//  {
+//    image: Fibo,
+//    role: "Linguistic researcher",
+ //   bio: `
+//   I am determined to break down language barriers and enhance communication in healthcare delivery to people who have sensory and cognitive impairments. My goal is to gain a comprehensive understanding of AI technologies, algorithms, and language models through intensive studies and courses in computational linguistics and Natural Language Processing.
+// By applying my knowledge in computational linguistics, I aspire to create innovative and intelligent technologies that can revolutionize healthcare services for those who are nonverbal, have hearing impairments, or suffer from language difficulties caused by brain disorders.
+  //  `,
+ //   name: "Asamoah Baffour Isaac ",
+//    quote: `“The world is a beautiful mosaic of colors, cultures, and perspectives. Each piece is unique and valuable, and together we create a masterpiece. Let us appreciate and embrace the diversity of our world, and not let fear or ignorance divide us."`,
+//    title: "Project Manager & linguistic researcher",
+//    link: 'https://www.linkedin.com/in/morro-suleyman-067462263/'
+    
+//  },
+  {
+    image: AsumVictor,
+    role: "Programming Lead",
+    bio: ` I am interested in pursuing computer science and Robotics, where I can combine my computational, especially Artificial Intelligence and engineering skills to solve societal problems. I am interested in developing artificial intelligence (AI) models to help translate Sign Languages to voice synthesis or text to bridge the communication gap between the hearing and the deaf community. I'm also curious to research computational agriculture where I will be making smart autonomous robots and precision agriculture to handle tedious and complex agricultural activities in Africa.
+Again, I wish to be on TEDx as a public speaker.`,
+    name: "Asum Victor",
+    quote: `My greatest accomplishment isn't behind me—it's what lies ahead. Why would I halt my efforts when there's always more to achieve?`,
+    title: "Fullstack Developer",
+    link: 'https://www.linkedin.com/in/victorasumdev/'
+
+  },
+//  {
+//    image: Prince,
+//    role: "Research Team Lead",
+//    bio: `I am highly enthusiastic about pursuing an interdisciplinary advancement in Electrical Engineering and Computer Engineering, complemented by a sub-discipline in sociology. My passion lies in utilizing technology to tackle the problem of digital inequality that affects individuals with disabilities. Specifically, I aim to focus on integrating artificial intelligence and machine learning within the design of assistive technologies, with the ultimate goal of bridging the accessibility gap for these individuals while understanding the struggles faced by disabled persons through sociology. Here, I can combine my leadership, critical thinking, problem-solving skills, and understanding of social dynamics with my taste for electrical systems, computing infrastructure, and human assistive technologies to develop holistic approaches to tackle digital inequality in African societies.`,
+//    name: "Amoah Prince Kofi Attah",
+//    quote: `Concentrate on the abilities your disability doesn't hinder, and don't dwell on the things it interferes with. Be disabled physically, not in spirit. `,
+//    title: "Researcher",
+//    link: 'https://www.linkedin.com/in/victorasumdev/'
+    
+//  },
+//  {
+//    image: Morro,
+//    role: "Data Analyst",
+//    bio: `Passionate about business and economics, I aim to excel in Business Administration while aspiring to become a top economist and successful entrepreneur. With a deep interest in analyzing market trends and economic policies, I aim to leverage this knowledge to drive positive change and create socially responsible businesses. Through practical experiences and academic pursuits, I am honing my skills to navigate the complexities of the business world and contribute to both economic growth and societal welfare.
+    
+  //  `,
+  //  name: "Suleyman Morro",
+ //   quote: `Empowering economies through insightful analysis, and businesses through innovative entrepreneurship, I strive to create a harmonious synergy between economic growth and societal well-being. `,
+//    title: "Ashesi University",
+  //  link: 'https://www.linkedin.com/in/morro-suleyman-067462263/'
+    
+//  },
+];
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,71 @@
+import Hero from "components/hero/TwoColumnWithVideo";
+import Mission from "components/features/TwoColSingleFeatureWithStats2";
+import styled from "styled-components";
+import tw from "twin.macro";
+import PlayButton from "../components/buttons/videoPlayButton.js";
+import VideoPlayer from "components/video/VideoPlayer.jsx";
+import Future from "../images/video/future plan.mp4";
+import TeamMember from "../components/hero/TwoColumnWithFeaturesAndTestimonial.js";
+import Profile from "../images/Profile/105683075.jpeg";
+import { TeamMembers } from "data/teamMembers.js";
+
+function HomeScreen() {
+  const Header = tw.div`py-4 bg-[#ffffff]`;
+  const TextHeader = tw.h2` underline font-semibold text-primary-400 text-2xl md:text-3xl text-center`;
+  const VideoFram = tw.div`my-10 w-full py-2 px-3 lg:px-10 mt-10`;
+  const VideoFram2 = tw.div`my-10 w-full relative h-[15cm]`;
+  const VideoContainer = tw.div`bg-primary-200 h-[10cm] w-full rounded-md flex justify-center items-center`;
+  const FuturePlan = tw.div` w-full  px-5 py-10 bg-[#00000099] absolute h-full`;
+  const FutureHeader = tw.h2` text-gray-200 text-3xl md:text-5xl font-bold`;
+  const Para = tw.p` mt-5 text-gray-100 text-[16px] w-full lg:w-9/12 font-normal`;
+  const Button = tw.button` px-6 py-2 bg-white mt-10 font-normal rounded-md text-blue-800`;
+
+  return (
+    <Header className=" mt-10">
+      <Hero />
+      <TextHeader>About Us</TextHeader>
+      <Mission />
+    <TextHeader>How It Works</TextHeader>
+      <VideoFram>
+        <VideoContainer>
+          <PlayButton />
+        </VideoContainer>
+      </VideoFram>
+      <VideoFram2>
+        <FuturePlan>
+          <FutureHeader tw=" text-white">
+            Future of digital connection
+          </FutureHeader>
+          <Para>
+            Our goal is to create a future where everyone, regardless of how
+            they communicate, feels understood and connected. We aim to break
+            down barriers between sign and spoken language, fostering a world
+            where deaf individuals can easily interact and share their stories
+            with others.
+          </Para>
+          <Button>Learn more</Button>
+        </FuturePlan>
+        <VideoPlayer src={Future} className={"bg-red-400 px-20"} />
+      </VideoFram2>
+     
+      <TextHeader>Meet The Team</TextHeader>
+      {TeamMembers.map(member=>(
+
+      <TeamMember
+        imageSrc={member.image}
+        heading={member.role}
+        description={member.bio}
+        primaryButtonUrl = {member.link}
+        testimonial={{
+          customerCompany: member.title,
+          customerName: member.name,
+          quote: member.quote,
+        }}
+      />
+      ))}
+
+    </Header>
+  );
+}
+
+export default HomeScreen;
